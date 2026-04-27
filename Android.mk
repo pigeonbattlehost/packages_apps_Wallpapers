3b6365095ac5d9ddf402188f77134198bd340e10
zntsproj <vseokaktusah7@gmail.com>
2026-04-27 22:44:26 +0300

    Wallpapers: initial commit

---
+++ Android.mk
@@ -0,0 +1,11 @@
+# Android APK Makefile for Wallpapers
+
+LOCAL_PATH := $(my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := Wallpapers
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := Wallpapers.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
